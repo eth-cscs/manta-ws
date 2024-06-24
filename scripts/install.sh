61313bfaeb311f1ced5c24588b1d61cddee8cc0e
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2024-06-24 21:02:03 +0200

    feat: update cama version

---
+++ scripts/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# copy config file
+scp ./scripts/config.toml root@hashicorp-vault:/root/.config/manta/config.toml
+
+# copy cert files
+scp ./scripts/alps_root_cert.pem root@hashicorp-vault:/root/.config/manta/alps_root_cert.pem
+
+# copy systemd unit file
+scp ./scripts/cama.service root@hashicorp-vault:/etc/systemd/system/cama.service
+
+# copy cama binary
+scp ./target/x86_64-unknown-linux-musl/release/cama root@hashicorp-vault:/usr/local/bin/cama
+
+ssh root@hashicorp-vault systemctl daemon-reload
+
+ssh root@hashicorp-vault systemctl enable cama.service
+
+ssh root@hashicorp-vault systemctl restart cama.service
